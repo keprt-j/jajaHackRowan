d4ee2eac66a0ff45a3eb49bde2ae1c5863567b5f
AadityaMunjal <aadityamunjal06@gmail.com>
2025-03-29 14:35:12 -0400

    connect model endpoint

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -5,9 +5,38 @@ import React from 'react';
 
 export default function Home() {
   const { user, isLoading } = useUser();
+
+  const fileInputRef = React.useRef<HTMLInputElement>(null);
+
+  const getPrediction = async () => {
+    console.log(fileInputRef.current?.files);
+    const formData = new FormData();
+    if (fileInputRef.current?.files) {
+      const file = fileInputRef.current.files[0];
+      formData.append('file', file);
+    }
+
+    console.log(formData);
+    try {
+      const response = await fetch('/api/upload', {
+        method: 'POST',
+        body: formData
+      });
+
+      if (!response.ok) {
+        throw new Error(await response.text());
+      }
+
+      const data = await response.json();
+      console.log('Upload success:', data);
+    } catch (err) {
+      console.error('Upload error:', err);
+    } finally {
+    }
+  };
   return (
     <div>
-      <div>
+      <div className="flex">
         {!isLoading && !user && (
           <div>
             <a href="/auth/login">Log in</a>
@@ -34,6 +63,24 @@ export default function Home() {
             </div>
           </div>
         )}
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            fileInputRef.current?.click();
+          }}
+          className="">
+          Upload Photo
+        </button>
+        <input
+          type="file"
+          className=""
+          hidden
+          onChange={getPrediction}
+          multiple={false}
+          ref={fileInputRef}
+          accept=".jpg, .jpeg, .png"
+        />
       </div>
     </div>
   );
---
+++ web/utils/consts.ts
@@ -0,0 +1,1 @@
+export const SERVER_URL = 'http://44.195.83.77/';
